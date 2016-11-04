9a1a251df63a243013849925422defc80fbb6fef
nesger <nesger.zhan@dji.com>
2016-11-04 15:56:36 +0800

    键盘工具类

---
+++ util/KeyBoardUtil.java
@@ -0,0 +1,40 @@
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Created by nesger.zhan on 2016/11/1.
+ */
+public class KeyBoardUtil {
+    /**
+     * 显示键盘
+     * @param edt
+     */
+    public static void show(final EditText edt){
+        edt.setFocusable(true);
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                InputMethodManager inputManager = (InputMethodManager) edt.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputManager.showSoftInput(edt, 0);
+            }
+        }, 500);
+    }
+
+    /**
+     * 隐藏键盘
+     * @param activity
+     */
+    public static void hide(Activity activity){
+        InputMethodManager manager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (activity.getWindow().getAttributes().softInputMode != WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN) {
+            if (activity.getCurrentFocus() != null)
+                manager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+}
